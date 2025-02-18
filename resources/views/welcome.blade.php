8d4083117e425a7d1e9fef13035f066e02d763a9
Katlego-Ram2 <rampedikatleho@gmail.com>
2025-02-18 10:47:44 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>K2 Auto Connect</title>
+</head>
+<body>
+   jjjjjjj 
+</body>
+</html>
